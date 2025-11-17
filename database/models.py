2d2130f0abf302ad2041b09e37a1200f388f4555
felipetorres-ft <240832410+felipetorres-ft@users.noreply.github.com>
2025-11-16 20:55:41 -0500

    fix: add autoincrement=True to Message.id field

--- database/models.py
+++ database/models.py
@@ -152,7 +152,7 @@ class Message(Base):
     """
     __tablename__ = "messages"
     
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     conversation_id = Column(Integer, ForeignKey("conversations.id"), nullable=False, index=True)
     
     # Conteúdo
